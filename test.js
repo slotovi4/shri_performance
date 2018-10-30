348aa115ea731dbfb9980c31d409b815fc781926
unknown <roman.bruns@mail.ru>
2018-10-30 17:38:40 +0300

    min sh

---
+++ test.js
@@ -0,0 +1,33 @@
+var webp = require("webp-converter");
+
+webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 50", function(
+  status
+) {
+  console.log(status);
+});
+
+webp.cwebp(
+  "assets/old/cloud-drizzle.png",
+  "assets/old/cloud-drizzle.webp",
+  "-q 50",
+  function(status) {
+    console.log(status);
+  }
+);
+
+webp.cwebp("assets/old/logo.png", "assets/old/logo.webp", "-q 50", function(
+  status
+) {
+  console.log(status);
+});
+webp.cwebp("assets/old/sh.png", "assets/old/sh.webp", "-q 50", function(
+  status
+) {
+  console.log(status);
+});
+
+webp.cwebp("assets/old/sh22.jpg", "assets/old/sh22.webp", "-q 50", function(
+  status
+) {
+  console.log(status);
+});
