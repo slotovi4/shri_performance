30e2eaaacff8e5ec3ede43e1a6d2d6cd3fbd2db6
unknown <roman.bruns@mail.ru>
2018-10-31 20:14:49 +0300

    opt logo

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 var webp = require("webp-converter");
 
-webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 9", function(
-  status
-) {
-  console.log(status);
-});
-
 webp.cwebp(
+  "assets/old/newbanner.jpg",
+  "assets/old/newbanner.webp",
+  "-q 100",
+  function(status) {
+    console.log(status);
+  }
+);
+
+/* webp.cwebp(
   "assets/old/cloud-drizzle.png",
   "assets/old/cloud-drizzle.webp",
   "-q 9",
@@ -15,7 +18,7 @@ webp.cwebp(
   }
 );
 
-webp.cwebp("assets/old/logo.png", "assets/old/logo.webp", "-q 9", function(
+webp.cwebp("assets/old/logo.png", "assets/old/logo2.webp", "-q 10", function(
   status
 ) {
   console.log(status);
@@ -29,3 +32,4 @@ webp.cwebp("assets/old/sh22.jpg", "assets/old/sh22.webp", "-q 9", function(
 ) {
   console.log(status);
 });
+ */
