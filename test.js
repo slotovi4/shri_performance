cb938230c3da864ca783b9ce452cdd3bb0c4e10f
unknown <roman.bruns@mail.ru>
2018-10-31 18:20:30 +0300

    rem unicode css & min sh

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var webp = require("webp-converter");
 
-webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 10", function(
+webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 9", function(
   status
 ) {
   console.log(status);
@@ -9,24 +9,22 @@ webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 10", function(
 webp.cwebp(
   "assets/old/cloud-drizzle.png",
   "assets/old/cloud-drizzle.webp",
-  "-q 10",
+  "-q 9",
   function(status) {
     console.log(status);
   }
 );
 
-webp.cwebp("assets/old/logo.png", "assets/old/logo.webp", "-q 10", function(
+webp.cwebp("assets/old/logo.png", "assets/old/logo.webp", "-q 9", function(
   status
 ) {
   console.log(status);
 });
-webp.cwebp("assets/old/sh.png", "assets/old/sh.webp", "-q 10", function(
-  status
-) {
+webp.cwebp("assets/old/sh.png", "assets/old/sh.webp", "-q 9", function(status) {
   console.log(status);
 });
 
-webp.cwebp("assets/old/sh22.jpg", "assets/old/sh22.webp", "-q 10", function(
+webp.cwebp("assets/old/sh22.jpg", "assets/old/sh22.webp", "-q 9", function(
   status
 ) {
   console.log(status);
