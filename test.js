18bb1fd5851232446046cd929b26cf082ae684ba
unknown <roman.bruns@mail.ru>
2018-10-31 19:40:02 +0300

    min logo

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var webp = require("webp-converter");
 
-webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 8", function(
+webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 9", function(
   status
 ) {
   console.log(status);
@@ -9,22 +9,22 @@ webp.cwebp("assets/old/banner.jpg", "assets/old/banner.webp", "-q 8", function(
 webp.cwebp(
   "assets/old/cloud-drizzle.png",
   "assets/old/cloud-drizzle.webp",
-  "-q 8",
+  "-q 9",
   function(status) {
     console.log(status);
   }
 );
 
-webp.cwebp("assets/old/logo.png", "assets/old/logo.webp", "-q 8", function(
+webp.cwebp("assets/old/logo.png", "assets/old/logo.webp", "-q 9", function(
   status
 ) {
   console.log(status);
 });
-webp.cwebp("assets/old/sh.png", "assets/old/sh.webp", "-q 8", function(status) {
+webp.cwebp("assets/old/sh.png", "assets/old/sh.webp", "-q 9", function(status) {
   console.log(status);
 });
 
-webp.cwebp("assets/old/sh22.jpg", "assets/old/sh22.webp", "-q 8", function(
+webp.cwebp("assets/old/sh22.jpg", "assets/old/sh22.webp", "-q 9", function(
   status
 ) {
   console.log(status);
